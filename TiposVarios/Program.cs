47a9f1d2d8e6bb2a3335b312366c4f7d69e96532
Inês Nunes <ines.perdigao.nunes@gmail.com>
2018-02-22 10:34:15 +0000

    Adicionar mais strings

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -7,8 +7,14 @@ using System.Threading.Tasks;
 namespace TiposVarios {
     class Program {
         static void Main(string[] args) {
-            string s = "Oh look it a string \u0CA5 n \u0CA5";
+            string s = "Oh look it a string \u0CA5 \u0C69 \u0CA5";
+            string v = @"Oh look it an ugly string \u0CA5 \u0C69 \u0CA5";
+            string c = "a" + 2;
+            string d = $"{5} mais {5} e igual a {5 + 5}";
             Console.WriteLine(s);
+            Console.WriteLine(v);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
             Console.ReadKey();
         }
     }
