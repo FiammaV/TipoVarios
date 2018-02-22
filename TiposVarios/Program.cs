a2754f1a9b878a4104fb7b223298d1188bf0c928
Inês Nunes <ines.perdigao.nunes@gmail.com>
2018-02-22 10:11:19 +0000

    Adicionar strings

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 namespace TiposVarios {
     class Program {
         static void Main(string[] args) {
+            string s = "Oh look it a string \u0CA5 n \u0CA5";
+            Console.WriteLine(s);
+            Console.ReadKey();
         }
     }
 }
